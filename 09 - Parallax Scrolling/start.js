7ea9fdd59a05e652cc162cf81482e2db52b4ac81
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-04-05 23:26:14 +0700

    Complete EP.9

--- 09 - Parallax Scrolling/start.js
+++ 09 - Parallax Scrolling/start.js
@@ -1,4 +1,17 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  function onScroll() {
+    const moonElem = document.querySelector(".moon");
+    const wishElem = document.querySelector(".wish");
 
+    moonElem.style.transform = `translate(${window.scrollY * 0.7}%, ${
+      window.scrollY * -0.7
+    }%)`;
+    wishElem.style.transform = `translateY(${window.scrollY * -1.2}%)`;
+  }
+
+  function run() {
+    document.addEventListener("scroll", onScroll);
+  }
+
+  run();
 })();
