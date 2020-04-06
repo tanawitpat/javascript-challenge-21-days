77cc753837dbb7ece137d73bb3221539b7cc15b4
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-04-06 22:05:44 +0700

    Complete EP.11

--- 11 - Text Reveal/start.js
+++ 11 - Text Reveal/start.js
@@ -1,4 +1,23 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  function onScroll() {
+    const sectionElems = Array.from(document.querySelectorAll("section"));
 
+    sectionElems.forEach((sectionElem) => {
+      const imgElem = sectionElem.querySelector("img");
+      const textElem = sectionElem.querySelector(".text");
+
+      const scrollPosition = window.pageYOffset;
+      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;
+
+      if (scrollPosition > revealPosition) {
+        textElem.classList.add("reveal");
+      }
+    });
+  }
+
+  function run() {
+    document.addEventListener("scroll", onScroll);
+  }
+
+  run();
 })();
