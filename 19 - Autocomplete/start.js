94546f10e715c8357d29c3af64e2dfc8fd14afa7
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-04-07 22:44:51 +0700

    Complete EP.19

--- 19 - Autocomplete/start.js
+++ 19 - Autocomplete/start.js
@@ -1,3 +1,52 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const carBrands = [
+    "BMW",
+    "Maserati",
+    "Mercedes Benz",
+    "Ferrari",
+    "Toyota",
+    "Honda",
+    "Hyundai",
+  ];
+
+  const searchElem = document.querySelector(".search");
+
+  function clearResults() {
+    const ulElem = document.querySelector(".results");
+    if (ulElem) {
+      document.body.removeChild(ulElem);
+    }
+  }
+
+  function selectCarBrand(event) {
+    searchElem.value = event.target.innerText;
+  }
+
+  function onInput(event) {
+    clearResults();
+
+    const inputText = event.target.value.toLowerCase();
+    const matchedCarBrands = carBrands.filter((carBrand) =>
+      carBrand.toLowerCase().startsWith(inputText)
+    );
+
+    const ulElem = document.createElement("ul");
+    ulElem.classList.add("results");
+
+    matchedCarBrands.forEach((carBrand) => {
+      const liElem = document.createElement("li");
+      liElem.innerText = carBrand;
+      liElem.onclick = selectCarBrand;
+      ulElem.appendChild(liElem);
+    });
+
+    document.body.appendChild(ulElem);
+  }
+
+  function run() {
+    searchElem.addEventListener("input", onInput);
+    document.addEventListener("click", clearResults);
+  }
+
+  run();
 })();
