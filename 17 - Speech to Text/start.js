3c0c247ed6fd56deb01da419af8e24c1d34d8d2b
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-04-07 22:13:59 +0700

    Complete EP.17

--- 17 - Speech to Text/start.js
+++ 17 - Speech to Text/start.js
@@ -1,3 +1,45 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new window.SpeechRecognition();
+  const btnElem = document.querySelector(".control");
+
+  function onClick() {
+    const isPausing = btnElem.classList.contains("record");
+
+    if (isPausing) {
+      recognition.start();
+      btnElem.classList.remove("record");
+      btnElem.classList.add("pause");
+    } else {
+      recognition.stop();
+      btnElem.classList.remove("pause");
+      btnElem.classList.add("record");
+    }
+  }
+
+  function onResult(event) {
+    const textElem = document.querySelector(".text");
+    const { transcript } = event.results[0][0];
+    textElem.innerText += transcript;
+  }
+
+  function onEnd() {
+    const isRecording = btnElem.classList.contains("pause");
+
+    if (isRecording) {
+      recognition.start();
+    }
+  }
+
+  function run() {
+    recognition.lang = "th-TH";
+
+    recognition.addEventListener("result", onResult);
+    recognition.addEventListener("end", onEnd);
+    btnElem.addEventListener("click", onClick);
+  }
+
+  run();
 })();
