749550cba7e53b6f03e9b770d2261e22f70bcfec
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-04-07 22:00:56 +0700

    Complete EP.16

--- 16 - Text to Speech/start.js
+++ 16 - Text to Speech/start.js
@@ -1,4 +1,23 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const message = new SpeechSynthesisUtterance();
 
+  function onVoicesChanged() {
+    const voices = speechSynthesis.getVoices();
+    const thVoice = voices.find((voice) => voice.lang === "th-TH");
+    message.voice = thVoice;
+  }
+
+  function onClick(event) {
+    message.text = event.target.getAttribute("alt");
+    speechSynthesis.speak(message);
+  }
+
+  function run() {
+    speechSynthesis.addEventListener("voiceschanged", onVoicesChanged);
+
+    const imgElems = Array.from(document.querySelectorAll("img"));
+    imgElems.forEach((imgElem) => imgElem.addEventListener("click", onClick));
+  }
+
+  run();
 })();
