d8021f4320d3938b57e4db8b973c3aa634d497d9
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-04-07 21:12:49 +0700

    Complete EP.14

--- 14 -  Carousel/start.js
+++ 14 -  Carousel/start.js
@@ -1,4 +1,34 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  let currentIndex = 0;
 
+  function displayImage(imageElems, newIndex) {
+    const lastIndex = imageElems.length - 1;
+
+    if (newIndex < 0) {
+      newIndex = lastIndex;
+    } else if (newIndex > lastIndex) {
+      newIndex = 0;
+    }
+
+    const newImage = imageElems[newIndex];
+    newImage.scrollIntoView({ behavior: "smooth" });
+
+    currentIndex = newIndex;
+  }
+
+  function run() {
+    const imageElems = document.querySelectorAll("img");
+    const previousBtmElem = document.querySelector(".previous");
+    const nextBtnElem = document.querySelector(".next");
+
+    previousBtmElem.addEventListener("click", () =>
+      displayImage(imageElems, currentIndex - 1)
+    );
+
+    nextBtnElem.addEventListener("click", () =>
+      displayImage(imageElems, currentIndex + 1)
+    );
+  }
+
+  run();
 })();
